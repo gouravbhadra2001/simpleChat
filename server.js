4443622579a74ccba0d2b1df11d28d634ab2409e
Gourav Bhadra <118394124+gouravbhadra2001@users.noreply.github.com>
2024-07-01 21:57:17 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const socketIO = require('socket.io');
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+
+  socket.on('messaged', (args) => {
+    io.emit('message', args);
+    console.log(args);
+  });
+});
